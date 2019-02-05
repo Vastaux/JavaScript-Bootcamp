28b7b80f3739f6ceec27a637c493bf58f54db99a
Stuart Brown <sbrown91@live.co.uk>
2019-02-05 17:53:49 +0000

    Progressed to lecture 14

---
+++ basics/string.js
@@ -0,0 +1,10 @@
+let firstName = 'Stuart';
+let lastName = 'Brown'
+let fullName = firstName + ' ' + lastName
+console.log(fullName);
+
+let city = 'Northampton, '
+let country = 'England'
+let location = city + country
+
+console.log("My name is:" + ' ' + fullName + " from " + location)
---
+++ basics/temp-converter.js
@@ -0,0 +1,7 @@
+let fahrenheit = 10
+let celcius = (fahrenheit - 32) * (5 / 9)
+let kelvin = (fahrenheit + 459.67) * (5 / 9)
+
+console.log("The temperature is " + fahrenheit + " Fahrenheit")
+console.log("This is " + celcius + " Celcius")
+console.log("and " + kelvin + " Kelvin")
