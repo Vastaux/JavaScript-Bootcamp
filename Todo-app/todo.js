f8001f562c6e22859815e427830fcc7ceabb70a1
Stuart Brown <sbrown91@live.co.uk>
2019-06-22 09:06:30 +0000

    Lecture 52 - Splite notes and todo into own projects

---
+++ Todo-app/todo.js
@@ -0,0 +1,31 @@
+class Todo {
+    constructor(text, completed) {
+        this.text = text,
+            this.completed = completed
+    }
+}
+
+const todo1 = new Todo('Order cat food', false)
+const todo2 = new Todo('Clean kitchen', true)
+const todo3 = new Todo('Buy food', true)
+const todo4 = new Todo('Do work', false)
+const todo5 = new Todo('Exercise', true)
+
+
+const todos = [todo1, todo2, todo3, todo4, todo5]
+
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed;
+})
+
+
+const summary = document.createElement('h3')
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector('body').appendChild(summary)
+
+
+todos.forEach( (todo) => {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
