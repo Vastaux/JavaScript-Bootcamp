261bd734e5af6ffddffea5d163b730af7dce58fb
Stuart Brown <sbrown91@live.co.uk>
2019-06-23 11:42:58 +0000

    Updated Notes-app functionality to have parity with Lecture 55.

--- Notes-app/notes.js
+++ Notes-app/notes.js
@@ -12,6 +12,16 @@ const notes = [{
     body: 'Get new chair'
 }]
 
-document.querySelector('button').addEventListener('click', function () {
-   console.log(notes) 
+document.querySelector('#create-note').addEventListener('click', function (e) {
+   e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#remove-all').addEventListener('click', function () {
+    document.querySelectorAll('.note').forEach(function(note) {
+        note.remove();
+    })
+})
+
+document.querySelector('#search-text').addEventListener('change', function(e) {
+    console.log(e)
 })
